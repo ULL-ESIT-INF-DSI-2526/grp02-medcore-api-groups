e5f9544c19e9df3cfab2d002f1db112c47bf0b91
gabmartinbr <alu0101539157@ull.edu.es>
2026-05-06 20:36:50 +0000

    fx models

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,12 @@
 import express from "express";
 import "./db/mongoose.js";
-import { medicationRouter } from "./routers/medications.js";
-import { patientRouter } from "./routers/patients.js";
-import { defaultRouter } from "./routers/default.js";
 import { staffRouter } from "./routers/staff.js";
+// import { patientRouter } from "./routers/patients.js";
+// import { defaultRouter } from "./routers/default.js";
 
 export const app = express();
 app.use(express.json());
-app.use(patientRouter);
+// app.use(patientRouter);
 app.use(staffRouter);
-app.use(medicationRouter);
-app.use(defaultRouter);
+
+// app.use(defaultRouter);
