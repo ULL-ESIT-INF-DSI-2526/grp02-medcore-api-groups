fad096f2450483c55cea675b804847326ef27996
Antonio Martinez Aleman <alu0101548029@ull.edu.es>
2026-05-10 17:08:50 +0100

    docs: add Swagger/OpenAPI 3.0 documentation
    - Static OpenAPI spec at src/docs/swagger.ts covering all endpoints
      (/patients, /staff, /medications, /records) with schemas, parameters,
      request bodies, response codes and examples.
    - Mount Swagger UI at /api-docs and raw spec at /api-docs.json in app.ts.
    - Add swagger-ui-express + swagger-jsdoc dependencies.

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,26 @@
 
 import express from "express";
+import swaggerUi from "swagger-ui-express";
 import "./db/mongoose.js";
 import { medicationRouter } from "./routers/medications.js";
 import { patientRouter } from "./routers/patients.js";
 import { defaultRouter } from "./routers/default.js";
 import { staffRouter } from "./routers/staff.js";
 import { recordRouter } from "./routers/records.js";
+import { swaggerSpec } from "./docs/swagger.js";
 
 export const app = express();
 app.use(express.json());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec, {
+  customSiteTitle: "MedCore API - Documentación",
+  swaggerOptions: { persistAuthorization: true },
+}));
+app.get("/api-docs.json", (_req, res) => {
+  res.setHeader("Content-Type", "application/json");
+  res.send(swaggerSpec);
+});
+
 app.use(patientRouter);
 app.use(staffRouter);
 app.use(medicationRouter);
